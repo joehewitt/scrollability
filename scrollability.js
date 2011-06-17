1b5ed073e609ee6ff0d0d446ab4c47b42ff71725
Joe Hewitt <joe@joehewitt.com>
2011-06-17 00:40:42 -0500

    * Tweak delay some more

--- scrollability.js
+++ scrollability.js
@@ -77,7 +77,7 @@ window.scrollability = {
                     target.updater(orig + ((y-orig) * (t/animationTime)));
                     if (++t > animationTime) {
                         clearInterval(animationInterval);
-                        setTimeout(stopAnimation, 300);
+                        setTimeout(stopAnimation, 200);
                     }
                 }, 20);
             } else {
